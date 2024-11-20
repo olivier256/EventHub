e92c0f91458e4a3ac5a01eff19598fc3d81e1e6e
olivier256 <60171288+olivier256@users.noreply.github.com>
2024-11-20 23:56:41 +0100

    Add files via upload

---
+++ javax/jms/DeliveryMode.java
@@ -0,0 +1,11 @@
+package javax.jms;
+
+public class DeliveryMode {
+
+	private DeliveryMode() {
+
+	}
+
+	public static final int PERSISTENT = 0;
+
+}
---
+++ javax/jms/JMSException.java
@@ -0,0 +1,7 @@
+package javax.jms;
+
+public class JMSException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
